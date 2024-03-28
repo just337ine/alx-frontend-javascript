47cde4939ca5a5d64819caf3af90a1d07b1aa87f
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-03-28 15:11:15 +0100

    task 10 solution

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
+  }
+  return newArray;
+}
