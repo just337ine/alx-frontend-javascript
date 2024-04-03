aab92bffed8b7ae5a00d0554d0a3282a827ee823
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-04-03 06:29:44 +0100

    task 3 solution

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSingup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((resolve) => {
+      console.log(
+        `${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`,
+      );
+    })
+    .catch(() => console.log('Signup system offlin'));
+}
