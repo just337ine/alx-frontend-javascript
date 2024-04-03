04a3da8133dc49c9775866b98d82fa0bd0ebe0ae
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-04-03 08:18:57 +0100

    task 10 solution

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default async function asyncUploadUser() {
+  let resolve = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    resolve = { photo, user };
+  } catch (error) {
+    resolve = { photo: null, user: null };
+  }
+  return resolve;
+}
