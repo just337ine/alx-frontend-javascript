5d43d5b072118efd65a7058040bc271fcd212805
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-04-08 03:32:45 +0100

    execution not successful

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,13 @@
 export const weakMap = new WeakMap();
 
+const MAX_ENDPOINT_CALLS = 5;
+
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
-  const count = weakMap.get(endpoint);
-  weakMap.set(endpoint, count + 1);
-  if (count >= 5) {
-    throw new Error('Endpoint load is high');
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error("Endpoint load is high");
   }
 }
